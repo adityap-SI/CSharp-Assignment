510eb516c4cf90d412f654e1deb41219d4a3b33f
Adiiityaaaaaaaaa <adityapandey241@gmail.com>
2023-08-15 17:40:18 +0530

    First Commit

---
+++ Day 2 Assignment/Library Management/Program.cs
@@ -0,0 +1,157 @@
+﻿namespace Library_Management
+{
+    class Book
+    {
+        public string Title { get; }
+        public string Author { get; }
+        public string Genre { get; }
+        public int QuantityAvailable { get; private set; }
+
+        public Book(string title, string author, string genre, int quantityAvailable)
+        {
+            Title = title;
+            Author = author;
+            Genre = genre;
+            QuantityAvailable = quantityAvailable;
+        }
+
+        public void AddQuantity(int quantity)
+        {
+            QuantityAvailable += quantity;
+        }
+
+        public void RemoveQuantity(int quantity)
+        {
+            if (QuantityAvailable >= quantity)
+                QuantityAvailable -= quantity;
+        }
+    }
+
+    class Library
+    {
+        private List<Book> books;
+
+        public Library()
+        {
+            books = new List<Book>();
+        }
+
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+        }
+
+        public void DisplayBookList()
+        {
+            if (books.Count == 0)
+            {
+                Console.WriteLine("The library has no books.");
+                return;
+            }
+
+            Console.WriteLine("Library Book List:");
+            foreach (Book book in books)
+            {
+                Console.WriteLine($"Title: {book.Title}, Author: {book.Author}, Genre: {book.Genre}, Quantity Available: {book.QuantityAvailable}");
+            }
+        }
+
+        public void SearchByTitle(string title)
+        {
+            List<Book> foundBooks = books.FindAll(book => book.Title.Contains(title, StringComparison.OrdinalIgnoreCase));
+
+            if (foundBooks.Count == 0)
+            {
+                Console.WriteLine($"No books found with the title: {title}");
+            }
+            else
+            {
+                Console.WriteLine($"Books found with the title: {title}");
+                foreach (Book book in foundBooks)
+                {
+                    Console.WriteLine($"Title: {book.Title}, Author: {book.Author}, Genre: {book.Genre}, Quantity Available: {book.QuantityAvailable}");
+                }
+            }
+        }
+
+        public void SearchByAuthor(string author)
+        {
+            List<Book> foundBooks = books.FindAll(book => book.Author.Contains(author, StringComparison.OrdinalIgnoreCase));
+
+            if (foundBooks.Count == 0)
+            {
+                Console.WriteLine($"No books found with the author: {author}");
+            }
+            else
+            {
+                Console.WriteLine($"Books found with the author: {author}");
+                foreach (Book book in foundBooks)
+                {
+                    Console.WriteLine($"Title: {book.Title}, Author: {book.Author}, Genre: {book.Genre}, Quantity Available: {book.QuantityAvailable}");
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Library library = new Library();
+
+            while (true)
+            {
+                Console.WriteLine("Enter 1 to add a book");
+                Console.WriteLine("Enter 2 to display the book list");
+                Console.WriteLine("Enter 3 to search for books by title");
+                Console.WriteLine("Enter 4 to search for books by author");
+                Console.WriteLine("Enter 5 to exit");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine("Enter the book's title:");
+                        string title = Console.ReadLine();
+
+                        Console.WriteLine("Enter the book's author:");
+                        string author = Console.ReadLine();
+
+                        Console.WriteLine("Enter the book's genre:");
+                        string genre = Console.ReadLine();
+
+                        Console.WriteLine("Enter the quantity of books available:");
+                        int quantity = Convert.ToInt32(Console.ReadLine());
+
+                        Book newBook = new Book(title, author, genre, quantity);
+                        library.AddBook(newBook);
+                        break;
+
+                    case "2":
+                        library.DisplayBookList();
+                        break;
+
+                    case "3":
+                        Console.WriteLine("Enter the title to search for:");
+                        string titleToSearch = Console.ReadLine();
+                        library.SearchByTitle(titleToSearch);
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Enter the author to search for:");
+                        string authorToSearch = Console.ReadLine();
+                        library.SearchByAuthor(authorToSearch);
+                        break;
+
+                    case "5":
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+        }
+    }
+
+}
---
+++ Day 2 Assignment/Store Management/Program.cs
@@ -0,0 +1,125 @@
+﻿namespace Store_Management
+{
+    class Product
+    {
+        public string Name { get; }
+        public double Price { get; }
+        public int QuantityInStock { get; private set; }
+        public string Category { get; }
+
+        public Product(string name, double price, int quantityInStock, string category)
+        {
+            Name = name;
+            Price = price;
+            QuantityInStock = quantityInStock;
+            Category = category;
+        }
+
+        public void AddQuantity(int quantity)
+        {
+            QuantityInStock += quantity;
+        }
+
+        public void RemoveQuantity(int quantity)
+        {
+            if (QuantityInStock >= quantity)
+                QuantityInStock -= quantity;
+        }
+    }
+
+    class Store
+    {
+        private List<Product> products;
+
+        public Store()
+        {
+            products = new List<Product>();
+        }
+
+        public void AddProduct(Product product)
+        {
+            products.Add(product);
+        }
+
+        public void DisplayProductList()
+        {
+            if (products.Count == 0)
+            {
+                Console.WriteLine("The store has no products.");
+                return;
+            }
+
+            Console.WriteLine("Product List:");
+            foreach (Product product in products)
+            {
+                Console.WriteLine($"Name: {product.Name}, Price: ${product.Price}, Quantity in Stock: {product.QuantityInStock}, Category: {product.Category}");
+            }
+        }
+
+        public double CalculateTotalValueInStock()
+        {
+            double totalValue = 0;
+
+            foreach (Product product in products)
+            {
+                totalValue += product.Price * product.QuantityInStock;
+            }
+
+            return totalValue;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Store store = new Store();
+
+            while (true)
+            {
+                Console.WriteLine("Enter 1 to add a product");
+                Console.WriteLine("Enter 2 to display the product list");
+                Console.WriteLine("Enter 3 to calculate the total value of products in stock");
+                Console.WriteLine("Enter 4 to exit");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine("Enter the product's name:");
+                        string name = Console.ReadLine();
+
+                        Console.WriteLine("Enter the product's price:");
+                        double price = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine("Enter the quantity of products in stock:");
+                        int quantityInStock = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Enter the product's category:");
+                        string category = Console.ReadLine();
+
+                        Product newProduct = new Product(name, price, quantityInStock, category);
+                        store.AddProduct(newProduct);
+                        break;
+
+                    case "2":
+                        store.DisplayProductList();
+                        break;
+
+                    case "3":
+                        double totalValueInStock = store.CalculateTotalValueInStock();
+                        Console.WriteLine($"Total value of products in stock: ${totalValueInStock}");
+                        break;
+
+                    case "4":
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+        }
+    }
+
+}
